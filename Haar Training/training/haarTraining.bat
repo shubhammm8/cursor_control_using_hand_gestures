ff960b64cd8ee7787f83c735d5cd68a947d4af58
Shubham Chaudhary <ShubhamChaudharymailme@gmail.com>
2017-03-18 14:14:58 +0530

    haar cascade training and implementation in the hand recognition project

---
+++ Haar Training/training/haarTraining.bat
@@ -0,0 +1,3 @@
+haartraining.exe -data cascades -vec vector/facevector.vec -bg negative/bg.txt -npos 550 -nneg 1100 -maxfalsealarm 0.5 -mem 1024 -mode ALL -w 40 -h 40 
+
+rem -nonsym
---
+++ Haar Training/training/samples_creation.bat
@@ -0,0 +1,1 @@
+createsamples.exe -info positive/info.txt -vec vector/facevector.vec -num 550 -maxxangle 1.1 -maxyangle 0 -maxzangle 0.5 -w 40 -h 40 
